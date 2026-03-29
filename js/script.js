64fc77a5471f28ac033b3763da95a188625b8841
Robson Cesar <rubinhodiz@gmail.com>
2026-03-29 17:57:54 -0300

    criando regras de recebimento dos elementos, adicionando evendo do submit para pegar os valores digitados

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("Ola mundo");
+//seleção dos elementos
+const multiplicationForm = document.querySelector("#multiplication-form"); // pega o elemento de FORM
+const numberInput = document.querySelector("#number"); //pega o elemento do INPUT DE NUMBER
+const multiplicationInput = document.querySelector("#multiplicator"); //Pega o elemento do INPUT do MULTIPLICADOR
+
+
+//funções
+
+//Eventos
+multiplicationForm.addEventListener("submit", (e) =>{
+    e.preventDefault(); //pra não recarregar a pagina
+
+    const multiplicationNumber = +numberInput.value; //pega do input acima o valor que o usuario digitou
+    const multiplicatorNumber = +multiplicationInput.value; // pega do input acima o multiplicador que o usuario digitou.
+
+    if(!multiplicationNumber || !multiplicatorNumber) return;
+    
+    console.log(`Numero digitado: ${multiplicationNumber} e multipilcador digitado: ${ multiplicatorNumber}`);
+
+
+});
