b4a13f5217ea140862b6482ca8cfe2d08ecfef08
Robson Cesar <rubinhodiz@gmail.com>
2026-03-29 20:32:22 -0300

    implementando a regra de gerar a tabuada, implementando botão limpar, ajustes css para botão limpar

--- js/script.js
+++ js/script.js
@@ -3,8 +3,40 @@ const multiplicationForm = document.querySelector("#multiplication-form"); // pe
 const numberInput = document.querySelector("#number"); //pega o elemento do INPUT DE NUMBER
 const multiplicationInput = document.querySelector("#multiplicator"); //Pega o elemento do INPUT do MULTIPLICADOR
 
+const multiplicationTable = document.querySelector("#multiplication-operations"); //irei usar esse elemento para zeraro o texto que mostra na div <p>Informe o número para calcular uma tabuada...</p>
+const multiplicationTitle = document.querySelector("#multiplation-title span"); // pegando o valor do numero digitado pra mostrarmos no SPAM
+
+const btlimpar = document.querySelector("#Btlimpar"); // criado para limpar a tabela
+
 
 //funções
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = ""; //zera o texto dessa CONST que pegamos acima.
+
+    //laço para pegar o multiplicador e varrer e exibir pra cada percorrido
+    for(i = 1; i <= multiplicatorNumber; i++){
+        const result = number * i;
+
+        //criei uma template 
+        const template = 
+        `<div class="row">
+                <div class="operation">${number} x ${i} = </div>
+                <div class="result">${result}</div>
+        </div>`;
+
+        const parser = new DOMParser();
+
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+
+        const row = htmlTemplate.querySelector(".row");
+
+        multiplicationTable.appendChild(row);
+
+    }
+    multiplicationTitle.innerText = number;
+    console.log(`Numero digitado: ${number} e multipilcador digitado: ${ multiplicatorNumber}`);
+};
+
 
 //Eventos
 multiplicationForm.addEventListener("submit", (e) =>{
@@ -15,7 +47,17 @@ multiplicationForm.addEventListener("submit", (e) =>{
 
     if(!multiplicationNumber || !multiplicatorNumber) return;
     
-    console.log(`Numero digitado: ${multiplicationNumber} e multipilcador digitado: ${ multiplicatorNumber}`);
-
+    
+    createTable(multiplicationNumber, multiplicatorNumber);
 
 });
+
+//criando evento, ao clicar em LIMPAR ele resetar e apenas colocar a mensagem abaixo
+btlimpar.addEventListener("click", () => {
+    numberInput.value = "";
+    multiplicationTitle.innerText = "";
+     multiplicationTable.innerHTML = "Informe o número para calcular uma tabuada"; //zera o texto dessa CONST que pegamos acima.
+     
+
+});
+
